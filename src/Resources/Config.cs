81eebe000e3243d639a9de34c9a9c795ef203fec
Esterhasz <smippy098@gmail.com>
2026-08-26 03:33:58 -0600

    add config

---
+++ src/Resources/Config.cs
@@ -0,0 +1,10 @@
+namespace mnswpr.Resources
+{
+    public static class Config
+    {
+        public static int MineCount { get; } = 99;
+
+        public static int FieldWidth { get; } = 20;
+        public static int FieldHeight { get; } = 20;
+    }
+}
